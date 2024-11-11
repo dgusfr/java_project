fc2270837c555dbaff4808fdd1d1b28f654a39b1
dgusfr <diegogustavo.fr@gmail.com>
2024-11-11 08:59:54 -0300

    feat(struct)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int hora;
+		System.out.printf("Quantas horas?", hora);
+	}
+
+}
