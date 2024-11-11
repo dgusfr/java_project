68e54f1498d58aed0ed8343a0c6926984ca229c0
dgusfr <diegogustavo.fr@gmail.com>
2024-11-11 09:02:37 -0300

    feat(add): horas

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		int hora;
-		System.out.printf("Quantas horas?", hora);
+		
+		System.out.printf("Quantas horas?");
+		hora = sc.nextInt();
 	}
 
 }
