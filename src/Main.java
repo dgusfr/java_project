83e9ce075cdd675c93adda85f59f58f8a30876b0
dgusfr <diegogustavo.fr@gmail.com>
2024-11-11 10:05:43 -0300

    feat(struct)

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,21 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int hora;
-		
-		System.out.printf("Quantas horas?");
-		hora = sc.nextInt();
-		
-		if(hora < 12) {
-			System.out.println("Bom dia");
-		} else {
-			if (hora < 18) {
-				System.out.println("Boa tarde");
-			}else {
-				System.out.println("Boa noite");
-			}
-		}
-		
-		sc. close();
-		
-	}
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double metroQuadrado = sc.nextDouble();
 
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+
+        System.out.printf("AREA = %.2f%n", area);
+        System.out.printf("PRECO = %.2f%n", preco);
+
+        sc.close();
+    }
 }
