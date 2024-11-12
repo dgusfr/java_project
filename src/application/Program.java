bdec551a9829263fae68a02272aed974626b206d
dgusfr <diegogustavo.fr@gmail.com>
2024-11-12 17:37:10 -0300

    add variables

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,23 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double xA, xB, xC, yA, yB, yC;
+		
+		System.out.println("Enter the mensure of the triangle X:");
+		xA = sc.nextDouble();
+		xB = sc.nextDouble();
+		xC = sc.nextDouble();
+		yA = sc.nextDouble();
+		yB = sc.nextDouble();
+		yC = sc.nextDouble();
 
 	}
 
