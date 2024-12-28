22e44bdfe1764605af2fe3a09b60ed2b30b146e1
dgusfr <diegogustavo.fr@gmail.com>
2024-12-28 11:23:00 -0300

    feat: Add JSONUtil for JSON manipulation

--- src/main/java/utils/JSONUtil.java
+++ src/main/java/utils/JSONUtil.java
@@ -1,5 +1,14 @@
-package main.java.utils;
+package com.project.app.utils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class JSONUtil {
-  
+    public static String formatJSONObject(JSONObject jsonObject) {
+        return jsonObject.toString(4);
+    }
+
+    public static JSONArray parseJSONArray(String jsonString) {
+        return new JSONArray(jsonString);
+    }
 }
