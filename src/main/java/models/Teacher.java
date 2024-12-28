40205317c8f236ffdd1a29ce1ae671c43de41d03
dgusfr <diegogustavo.fr@gmail.com>
2024-12-28 11:20:06 -0300

    feat: Add Teacher model class extending User

--- src/main/java/models/Teacher.java
+++ src/main/java/models/Teacher.java
@@ -1,5 +1,11 @@
-package main.java.models;
+package com.project.app.models;
 
-public class Teacher {
-  
+public class Teacher extends User {
+    public Teacher(String name, String email) {
+        super(name, email);
+    }
+
+    public String approveStudent(String studentName, String course) {
+        return "Student " + studentName + " approved for course " + course + ".";
+    }
 }
