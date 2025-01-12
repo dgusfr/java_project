98b94310de7bc6a40d43cdb9b1b7397adbe94c77
dgusfr <diegogustavo.fr@gmail.com>
2025-01-12 08:59:41 -0300

    changes

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,8 +6,11 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter account number:");
 		
 		sc.close();
 	}
