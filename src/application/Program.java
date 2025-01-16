ab7aef02655c0ec559139d29475e8394bd6eca66
dgusfr <diegogustavo.fr@gmail.com>
2025-01-16 19:41:58 -0300

    fix(): reafact code

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,31 @@
-package application;
 
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		double[] vect = new double[n];
-		
-		for(int i=0; i<=n; i++) {
-			vect[i] = sc.nextDouble();
-		}
-		
-		double sum = 0.0;
-		for(int i=0; i<=n; i++) {
-			sum = sum + vect[i];
-		}
-		
-		double avg = sum / n;
-		System.out.println("Average Height:", avg);
-				
-		sc.close();
-	}
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Adicione o tamanho do array:");
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+        
+        System.out.println("Adicione os valores do array:");
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+        
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+        
+        double avg = sum / n;
+        System.out.printf("Média das alturas: %.2f%n", avg);
+                
+        sc.close();
+    }
 }
