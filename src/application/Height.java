d431df635c0230707768bf82897ab646e115060a
dgusfr <diegogustavo.fr@gmail.com>
2025-01-17 08:46:44 -0300

    fix(): reafact code

--- src/application/Height.java
+++ src/application/Height.java
@@ -14,15 +14,12 @@ public class Height {
         
         System.out.println("Adicione o tamanho do array:");
         int n = sc.nextInt();
-        Double[] numeros;
-        numeros = new Double[n];
+        int[] vet = 
         
         System.out.println("Digite um numero:");
-        for (int i = 0; i < vect.length; i++) {
-        	sc.nextLine();
-        	String name = sc.nextLine();
+        for (int i = 0; i < numeros.length; i++) {
         	double price = sc.nextDouble();
-            vect[i] = new Product(name, price);
+            numeros[i] = new Product(name, price);
         }
         
         double sum = 0.0;
