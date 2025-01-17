0466d561b5475daf830f5567ec7bea623c3afc45
dgusfr <diegogustavo.fr@gmail.com>
2025-01-17 08:58:23 -0300

    fix(): reafact code

--- src/application/Negatives.java
+++ src/application/Negatives.java
@@ -5,27 +5,34 @@ import java.util.Scanner;
 
 public class Negatives {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Adicione o tamanho do array:");
         int n = sc.nextInt();
         int[] vet = new int[n];
-        
-        int negativos = 0; 
+
+        int negativos = 0;
+
         for (int i = 0; i < n; i++) {
-            System.out.println("Digite um numero:");
+            System.out.println("Digite um número:");
             vet[i] = sc.nextInt();
             if (vet[i] < 0) {
-                negativos++; 
-                System.out.println("Numeros negativos:");
+                negativos++;
             }
         }
+
+        System.out.println("Números negativos:");
+        for (int i = 0; i < n; i++) {
+            if (vet[i] < 0) {
+                System.out.println(vet[i]);
+            }
+        }
+
         System.out.println("Quantidade de números negativos: " + negativos);
-         
+
         sc.close();
     }
-
 }
