0e6d9a210c91be10ada251ffc8eb60148efcdd66
dgusfr <diegogustavo.fr@gmail.com>
2025-01-19 08:47:38 -0300

    feat(add): Soma1a1000

---
+++ src/application/Soma1a1000.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Soma1a1000 {
+
+	public static void main(String[] args) {
+		int soma = 0;
+		for(int i = 1; i <= 1000; i++) {
+			soma = soma + i;
+		}
+		System.out.println(soma);
+	}
+
+}
