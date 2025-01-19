e49be38430db16ca1b7348da58bb7ea00aa02f96
dgusfr <diegogustavo.fr@gmail.com>
2025-01-19 09:08:01 -0300

    feta add FibonacciAte100

--- src/application/FibonacciAte100.java
+++ src/application/FibonacciAte100.java
@@ -2,16 +2,17 @@ package application;
 
 public class FibonacciAte100 {
 
-	public static void main(String[] args) {
-		int anterior = 0;
-		int atual = 1;
-		
-		System.out.println("Série de Fibonacci: " + anterior);
-		
-		while (atual <= 100) {
-			System.out.println("," + atual);
-			int proximo = anterior + atual;
-		}
-	}
+    public static void main(String[] args) {
+        int anterior = 0;
+        int atual = 1;
 
+        System.out.print("Série de Fibonacci: " + anterior);
+
+        while (atual <= 100) {
+            System.out.print(", " + atual);
+            int proximo = anterior + atual;
+            anterior = atual;
+            atual = proximo;
+        }
+    }
 }
