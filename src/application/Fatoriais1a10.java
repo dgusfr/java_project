1a876e32b9937c7153e0f68ccce4cf3eabdd1891
dgusfr <diegogustavo.fr@gmail.com>
2025-01-19 08:55:10 -0300

    feat add

---
+++ src/application/Fatoriais1a10.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Fatoriais1a10 {
+
+	public static void main(String[] args) {
+		int fat = 1;
+		for(int i = 2; i <= 10; i++) {
+			fat = fat * i;
+		}
+		System.out.println("O fatorial de 10");
+	}
+}
