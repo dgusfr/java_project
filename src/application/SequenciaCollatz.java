bce24956029d7d1585fba78f029c3ce45d5d6d7d
dgusfr <diegogustavo.fr@gmail.com>
2025-02-23 23:14:41 -0300

    refact

--- src/application/SequenciaCollatz.java
+++ src/application/SequenciaCollatz.java
@@ -4,15 +4,24 @@ import java.util.Scanner;
 
 public class SequenciaCollatz {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Digite um numero:");
-		int numero = sc.nextInt();
-		for(int i = 1; i <= 1000; i++) {
-			soma = soma + i;
-		}
-		System.out.println(soma);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Digite um numero: ");
+        int numero = sc.nextInt();
 
-	}
+        System.out.println("Sequência de Collatz:");
+        while (numero > 1) {
+            System.out.println(numero);  
 
+            if (numero % 2 == 0) {
+                numero = numero / 2;      
+            } else {
+                numero = 3 * numero + 1;  
+            }
+        }
+
+        System.out.println(numero); 
+        sc.close();
+    }
 }
