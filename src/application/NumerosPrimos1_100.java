fb2d4b9ec32198c2ca9e8368bb14ef3ea68dd9ce
dgusfr <diegogustavo.fr@gmail.com>
2025-03-12 09:21:02 -0300

    feat add eh primo

--- src/application/NumerosPrimos1_100.java
+++ src/application/NumerosPrimos1_100.java
@@ -2,7 +2,17 @@ package application;
 
 public class NumerosPrimos1_100 {
   public static void main(String[] args) {
-
+    for (int i = 2; i <=100; i ++){
+      boolean primo = true;
+      for( int j = 2; j < i; j++){
+        if(i % j == 0){
+          primo = false;
+          break;
+        }
+      }
+      if (primo) {
+        System.out.println(i);
+      }
+    }
   }
-
 }
