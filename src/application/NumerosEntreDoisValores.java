561e8c51c628bd2ab1cf2b41a283e7009d25a7ee
dgusfr <diegogustavo.fr@gmail.com>
2025-03-12 09:07:08 -0300

    feat add numero entre dois valores

---
+++ src/application/NumerosEntreDoisValores.java
@@ -0,0 +1,25 @@
+package application;
+import java.util.Scanner;
+
+public class NumerosEntreDoisValores {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Digite o primeiro número inteiro: ");
+    int numero_um = sc.nextInt();
+
+    System.out.print("Digite o segundo número inteiro: ");
+    int numero_dois = sc.nextInt();
+
+    if (numero_um < numero_dois) {
+      for (int i = numero_um; i <=numero_dois; i++){
+        System.out.print(i + " ");
+    }
+    }else{
+      for (int i = numero_dois; i <=numero_um; i++){
+        System.out.print(i + " ");
+      }
+    }
+  sc.close();
+  }
+}
