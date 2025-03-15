24c8298b354a0541ed4eaf698908b7427caaa029
dgusfr <diegogustavo.fr@gmail.com>
2025-03-15 20:43:53 -0300

    feat(add): refact the code

---
+++ src/application/IMC/Main.java
@@ -0,0 +1,28 @@
+package application.IMC;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Idade: ");
+        int idade = sc.nextInt();
+        nextLine();
+
+        System.out.print("Altura: ");
+        double altura = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.print("Peso: ");
+        double peso = sc.nextDouble();
+        sc.nextLine();
+
+        Pessoa pessoa = new Pessoa(nome, idade, altura, peso);
+        pessoa.exibirDados();
+
+    }
+}
