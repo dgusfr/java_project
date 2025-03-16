2f704f0ead48ab8e9a2778952f993aca0511e149
dgusfr <diegogustavo.fr@gmail.com>
2025-03-16 10:43:22 -0300

    feat(add): implement Triangulo class with side attributes and Main class for user input

---
+++ src/application/avaliaTriangulo/Main.java
@@ -0,0 +1,27 @@
+package application.avaliaTriangulo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        System.out.println("Digite o valor do lado A: ");
+        double side_A = sc.nextDouble();
+
+        System.out.println("Digite o valor do lado B: ");
+        double side_B = sc.nextDouble();
+
+        System.out.println("Digite o valor do lado C: ");
+        double side_C = sc.nextDouble();
+
+        Triangulo triangulo = new Triangulo(side_A, side_B, side_C);
+
+
+
+        sc.close();
+
+}
+}
--- src/application/avaliaTriangulo/Triangulo.java
+++ src/application/avaliaTriangulo/Triangulo.java
@@ -1,5 +1,45 @@
 package application.avaliaTriangulo;
 
 public class Triangulo {
+  private double side_A;
+  private double side_B;
+  private double side_C;
+
+  public Triangulo(double side_A, double side_B, double side_C){
+    this.side_A = side_A;
+    this.side_B = side_B;
+    this.side_C = side_C;
+  }
+
+  public double getSide_A() {
+    return side_A;
+  }
+
+  public void setSide_A(double side_A) {
+    this.side_A = side_A;
+  }
+
+  public double getSide_B() {
+    return side_B;
+  }
+
+  public void setSide_B(double side_B) {
+    this.side_B = side_B;
+  }
+
+  public double getSide_C() {
+    return side_C;
+  }
+
+  public void setSide_C(double side_C) {
+    this.side_C = side_C;
+  }
+
+  
+
+
+
+
+  
 
 }
